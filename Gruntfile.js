05d934659b7fbd55b3cf8e24219405b76b2edaa6
Zack Chase <zack@distelli.com>
2015-11-18 15:22:36 -0800

    removed semicolons to fail JSHint grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,9 +34,5 @@ module.exports = function(grunt) {
   // we can only load these if they are in our package.json
   // make sure you have run npm install so our app can find these
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
 };
